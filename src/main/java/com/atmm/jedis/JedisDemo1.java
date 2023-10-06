62eab17e37a4f4bcfdd25ccce9ca4733cb816bee
mxh <793604760@qq.com>
2023-10-06 11:02:12 +0800

    Update AAA.java

--- src/main/java/com/atmm/jedis/JedisDemo1.java
+++ src/main/java/com/atmm/jedis/JedisDemo1.java
@@ -77,7 +77,7 @@ public class JedisDemo1 {
     }
 
     @Test
-    public void demo() {
+    public void demo9() {
         String i = null;
         if (i.equals("1")) {
             System.out.println(i);
